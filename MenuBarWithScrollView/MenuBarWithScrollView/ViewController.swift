b9442b92848476b3864dcfe627001b3e19ef4616
YounByoungil <jimmy7525@naver.com>
2022-05-22 23:48:39 +0900

    [feat] Setup Project

---
+++ MenuBarWithScrollView/MenuBarWithScrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MenuBarWithScrollView
+//
+//  Created by 윤병일 on 2022/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
