e3668f34caaf352fa98642b3806ccbae4c926e5f
YounByoungil <jimmy7525@naver.com>
2022-06-19 18:41:18 +0900

    [feat] Project Setting

---
+++ TopMenuBarWithCollectionView/TopMenuBarWithCollectionView/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  TopMenuBarWithCollectionView
+//
+//  Created by 윤병일 on 2022/06/19.
+//
+
+import UIKit
+
+
+class MainViewController : UIViewController {
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    configureUI()
+  }
+  
+  //MARK: - Functions
+  private func configureUI() {
+    
+  }
+  
+  //MARK: - @objc func
+  
+}
